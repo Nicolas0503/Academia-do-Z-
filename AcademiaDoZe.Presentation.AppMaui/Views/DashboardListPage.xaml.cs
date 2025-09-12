f0a3f14fe5c5f227f149a11bffc3546d844f3329
Nicolas <jubilevc@gmail.com>
2025-09-12 17:22:34 -0300

    muai, e resolver problemas de application

---
+++ AcademiaDoZe.Presentation.AppMaui/Views/DashboardListPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AcademiaDoZe.Presentation.AppMaui.Views;
+
+public partial class DashboardListPage : ContentPage
+{
+	public DashboardListPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AcademiaDoZe.Presentation.AppMaui/Views/LogradouroPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AcademiaDoZe.Presentation.AppMaui.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
